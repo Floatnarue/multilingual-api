ea13651a23775e27427a73f0556300cd4f398d1b
Floatnarue <naroebordin@gmail.com>
2024-12-14 12:55:04 +0700

    feat: database setup with prisma

---
+++ api-core/src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { DynamicModule } from '@nestjs/common'
+
+
+import { DatabaseService } from './database.service'
+
+
+export class DatabaseModule {
+    static forRoot(): DynamicModule {
+        return {
+            global: true,
+            module: DatabaseModule,
+            providers: [
+                DatabaseService,
+
+            ],
+            exports: [DatabaseService],
+        }
+    }
+}
